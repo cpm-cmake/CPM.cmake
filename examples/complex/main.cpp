5dad9d4e9d9ab42be1c45a5f65f0ceb04b939c29
Lars Melchior <TheLartians@users.noreply.github.com>
2019-05-10 16:20:53 +0200

    update examples (#45)

--- examples/complex/main.cpp
+++ examples/complex/main.cpp
@@ -1,29 +1,21 @@
 #include <lars/parser/extension.h>
-#include <lars/lua_glue.h>
+#include <glue/lua.h>
 #include <stdexcept>
+#include <iostream>
 
 int main() {
   // create lua state
-  auto lua = lars::LuaState();
-  lua.open_libs();
+  auto lua = glue::LuaState();
+  lua.openStandardLibs();
 
-  // create extensions
-  lars::Extension extensions;
-
-  // add parser library to extension
-  extensions.add_extension("parser", lars::extensions::parser());
-
-  // connect parser extension to lua
-  extensions.connect(lua.get_glue());
-
+  lua["parser"] = lars::glue::parser();
+  
   // create a parser
   lua.run(R"(
     NumberMap = parser.Program.create()
-
     NumberMap:setRule("Whitespace", "[ \t]")
     NumberMap:setSeparatorRule("Whitespace")
-
-    NumberMap:setRuleWithCallback("Object", "'{' KeyValue (',' KeyValue)* '}'",function(e) 
+    NumberMap:setRuleWithCallback("Object", "'{' KeyValue (',' KeyValue)* '}'",function(e)
       local N = e:size()-1
       local res = {}
       for i=0,N do 
@@ -32,21 +24,18 @@ int main() {
       end
       return res
     end)
-
     NumberMap:setRule("KeyValue", "Number ':' Number")
-
     NumberMap:setRuleWithCallback("Number", "'-'? [0-9]+", function(e) return tonumber(e:string()); end)
-    
     NumberMap:setStartRule("Object")
   )");
 
   // parse a string
   lua.run("m = NumberMap:run('{1:3, 2:-1, 3:42}')");
-  
+
   // check result
-  if (lua.get_numeric("m[1]") != 3) throw std::runtime_error("unexpected result");
-  if (lua.get_numeric("m[2]") != -1) throw std::runtime_error("unexpected result");
-  if (lua.get_numeric("m[3]") != 42) throw std::runtime_error("unexpected result");
+  if (lua.get<int>("m[1]") != 3) throw std::runtime_error("unexpected result");
+  if (lua.get<int>("m[2]") != -1) throw std::runtime_error("unexpected result");
+  if (lua.get<int>("m[3]") != 42) throw std::runtime_error("unexpected result");
 
   return 0;
 }
