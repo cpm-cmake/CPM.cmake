8253393c751ad9325df312012101320d9ddbf519
Lars Melchior <TheLartians@users.noreply.github.com>
2019-05-17 17:32:52 +0200

    add yaml example

---
+++ examples/yaml/main.cpp
@@ -0,0 +1,14 @@
+#include <yaml-cpp/yaml.h>
+#include <iostream>
+
+int main(int argc, char ** argv){
+  if (argc != 2) {
+    std::cout << "usage: " << argv[0] << " <path to yaml file>" << std::endl;
+    return 1;
+  }
+
+  YAML::Node config = YAML::LoadFile(argv[1]);
+  std::cout << "Parsed YAML:\n" << config << std::endl;
+
+  return 0;
+}
