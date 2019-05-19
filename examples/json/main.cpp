2cf300740c4c90f3fa09664566def91bf06f73b0
Lars Melchior <TheLartians@users.noreply.github.com>
2019-05-19 18:54:20 +0200

    added json example

---
+++ examples/json/main.cpp
@@ -0,0 +1,25 @@
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <iomanip>
+
+int main(){
+  
+  nlohmann::json json = {
+    {"pi", 3.141},
+    {"happy", true},
+    {"name", "Niels"},
+    {"nothing", nullptr},
+    {"answer", {
+      {"everything", 42}
+    }},
+    {"list", {1, 0, 2}},
+    {"object", {
+      {"currency", "USD"},
+      {"value", 42.99}
+    }}
+  };
+
+  std::cout << "declared JSON object: " << std::setw(2) << json << std::endl;
+
+  return 0;
+}
