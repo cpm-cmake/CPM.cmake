1e25367c8c2cde2bb7ee5371fcf44a2b21891a81
Lars Melchior <TheLartians@users.noreply.github.com>
2019-05-25 14:14:31 +0200

    add sol2 example (#65)
    * add sol2 example
    
    * Update README.md
    
    * Update README.md

---
+++ examples/sol2/main.cpp
@@ -0,0 +1,16 @@
+#include <sol/sol.hpp>
+#include <cassert>
+
+struct vars {
+    int boop = 0;
+};
+
+int main() {
+    sol::state lua;
+    lua.open_libraries( sol::lib::base );
+    lua.new_usertype<vars>("vars", "boop", &vars::boop);
+    lua.script("beep = vars.new()\n"
+               "beep.boop = 1\n"
+               "print('beep boop')");
+    assert(lua.get<vars>("beep").boop == 1);
+}
