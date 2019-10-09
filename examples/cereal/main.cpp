b1855e927554f82bc7951f400cc5eeb9cdb7e599
Paul T <DeveloperPaul123@users.noreply.github.com>
2019-10-09 09:30:59 -0400

    Feature/cereal example (#85)
    * Added GTest (googletest) Example
    
    The examples tests the Fibonacci library similar to the doctest example.
    
    * Add Example for the cereal library.
    
    * Update examples/cereal/CMakeLists.txt
    
    Co-Authored-By: Lars Melchior <TheLartians@users.noreply.github.com>
    
    * Remove Test Coverage Code
    
    * Remove Test Coverage Option

---
+++ examples/cereal/main.cpp
@@ -0,0 +1,34 @@
+#define CATCH_CONFIG_MAIN
+
+#include <cereal/cereal.hpp>
+#include <cereal/archives/json.hpp>
+
+#include <string>
+#include <sstream>
+
+struct player_data 
+{
+  int id{-1};
+  std::string name{};
+};
+
+template<typename Archive>
+void serialize(Archive& archive, player_data const &data)
+{
+  archive(
+    cereal::make_nvp("id", data.id), 
+    cereal::make_nvp("name", data.name)
+  );
+}
+
+int main(int argc, char const *argv[])
+{
+  player_data player{3, "Gamer One"};
+  std::ostringstream oss;
+  cereal::JSONOutputArchive output(oss);
+  output(cereal::make_nvp("player_data", player));
+
+  std::cout << oss.str() << std::endl;
+
+  return 0;
+}
