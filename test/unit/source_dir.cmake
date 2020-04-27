c6887c8732a64b2f341180f09b5438c1afb55718
Lars Melchior <TheLartians@users.noreply.github.com>
2020-04-27 12:03:05 +0200

    create FindXXX.cmake modules for added CPM packages

--- test/unit/source_dir.cmake
+++ test/unit/source_dir.cmake
@@ -4,15 +4,17 @@ include(${CPM_PATH}/testing.cmake)
 
 set(TEST_BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/source_dir)
 
+set(TEST_DEPENDENCY_NAME Dependency)
+
 configure_package_config_file(
-  "${CMAKE_CURRENT_LIST_DIR}/source_dir/CMakeLists.txt.in"
-  "${CMAKE_CURRENT_LIST_DIR}/source_dir/CMakeLists.txt"
+  "${CMAKE_CURRENT_LIST_DIR}/test_project/CMakeLists.txt.in"
+  "${CMAKE_CURRENT_LIST_DIR}/test_project/CMakeLists.txt"
   INSTALL_DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/junk
 )
 
 execute_process(
   COMMAND 
-  ${CMAKE_COMMAND} "-H${CMAKE_CURRENT_LIST_DIR}/source_dir" "-B${TEST_BUILD_DIR}"
+  ${CMAKE_COMMAND} "-H${CMAKE_CURRENT_LIST_DIR}/test_project" "-B${TEST_BUILD_DIR}"
   RESULT_VARIABLE ret
 )
 
