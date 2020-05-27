7a0aa04bb2df7298696a27166e9af736c0cfb7e1
Lars Melchior <TheLartians@users.noreply.github.com>
2020-05-27 18:49:38 +0200

    remove test package lock from git

--- test/unit/test_project/package-lock.cmake
+++
@@ -1,5 +0,0 @@
-# CPM Package Lock
-# This file should be committed to version control
-
-# Dependency
-CPMDeclarePackage(Dependency "NAME;Dependency;SOURCE_DIR;/Users/lars/Documents/Development/CPM/test/unit/test_project/dependency")
