8a0b326e05a604c69f5465bab9b29fbf2cb7019b
Lars Melchior <TheLartians@users.noreply.github.com>
2021-01-06 14:15:29 +0100

    rename test var

--- test/unit/modules.cmake
+++ test/unit/modules.cmake
@@ -3,7 +3,7 @@ include(${CPM_PATH}/testing.cmake)
 
 set(TEST_BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/modules)
 
-function(initProjectWithDependency TEST_DEPENDENCY_NAME)
+function(init_project_with_dependency TEST_DEPENDENCY_NAME)
   configure_package_config_file(
     "${CMAKE_CURRENT_LIST_DIR}/local_dependency/ModuleCMakeLists.txt.in"
     "${CMAKE_CURRENT_LIST_DIR}/local_dependency/CMakeLists.txt"
@@ -18,11 +18,11 @@ function(initProjectWithDependency TEST_DEPENDENCY_NAME)
   assert_equal(${ret} "0")
 endfunction()
 
-initprojectwithdependency(A)
+init_project_with_dependency(A)
 assert_exists(${TEST_BUILD_DIR}/CPM_modules)
 assert_exists(${TEST_BUILD_DIR}/CPM_modules/FindA.cmake)
 assert_not_exists(${TEST_BUILD_DIR}/CPM_modules/FindB.cmake)
 
-initprojectwithdependency(B)
+init_project_with_dependency(B)
 assert_not_exists(${TEST_BUILD_DIR}/CPM_modules/FindA.cmake)
 assert_exists(${TEST_BUILD_DIR}/CPM_modules/FindB.cmake)
