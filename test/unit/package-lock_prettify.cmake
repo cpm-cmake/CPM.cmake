412fff2af66b3c22b4fb019abf00e2c31f6d1c8f
Alexandre Salconi <asalconi@cimeq.qc.ca>
2021-01-25 15:27:11 -0500

    reformated unit test prettify

--- test/unit/package-lock_prettify.cmake
+++ test/unit/package-lock_prettify.cmake
@@ -3,33 +3,47 @@ cmake_minimum_required(VERSION 3.14 FATAL_ERROR)
 include(${CPM_PATH}/CPM.cmake)
 include(${CPM_PATH}/testing.cmake)
 
-
 unset(PRETTY_ARGN)
-cpm_prettify_package_arguments(PRETTY_ARGN false
-    NAME Dependency
-    SOURCE_DIR ${CMAKE_SOURCE_DIR}/local_dependency/dependency
-    UPDATE_DISCONNECTED ON
-    TESTCUSTOMDATA TRUE
+cpm_prettify_package_arguments(
+  PRETTY_ARGN
+  false
+  NAME
+  Dependency
+  SOURCE_DIR
+  ${CMAKE_SOURCE_DIR}/local_dependency/dependency
+  UPDATE_DISCONNECTED
+  ON
+  TESTCUSTOMDATA
+  TRUE
 )
-SET(EXPECTED_UNCOMMENTED "    NAME Dependency
+set(EXPECTED_UNCOMMENTED
+    "    NAME Dependency
     SOURCE_DIR \${CMAKE_SOURCE_DIR}/local_dependency/dependency
     #(unparsed)
-    UPDATE_DISCONNECTED ON TESTCUSTOMDATA TRUE 
-")
-ASSERT_EQUAL(${PRETTY_ARGN} ${EXPECTED_UNCOMMENTED})
-
+    UPDATE_DISCONNECTED ON TESTCUSTOMDATA TRUE
+"
+)
+assert_equal(${PRETTY_ARGN} ${EXPECTED_UNCOMMENTED})
 
 unset(PRETTY_ARGN)
-cpm_prettify_package_arguments(PRETTY_ARGN true
-    NAME Dependency
-    SOURCE_DIR ${CMAKE_SOURCE_DIR}/local_dependency/dependency
-    UPDATE_DISCONNECTED ON
-    TESTCUSTOMDATA TRUE
+cpm_prettify_package_arguments(
+  PRETTY_ARGN
+  true
+  NAME
+  Dependency
+  SOURCE_DIR
+  ${CMAKE_SOURCE_DIR}/local_dependency/dependency
+  UPDATE_DISCONNECTED
+  ON
+  TESTCUSTOMDATA
+  TRUE
 )
-SET(EXPECTED_COMMENTED "#    NAME Dependency
+set(EXPECTED_COMMENTED
+    "#    NAME Dependency
 #    SOURCE_DIR \${CMAKE_SOURCE_DIR}/local_dependency/dependency
     #(unparsed)
-#    UPDATE_DISCONNECTED ON TESTCUSTOMDATA TRUE 
-")
-ASSERT_EQUAL(${PRETTY_ARGN} ${EXPECTED_COMMENTED})
-#----
+#    UPDATE_DISCONNECTED ON TESTCUSTOMDATA TRUE
+"
+)
+assert_equal(${PRETTY_ARGN} ${EXPECTED_COMMENTED})
+# ----
