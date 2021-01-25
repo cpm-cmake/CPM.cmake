07c65db6e86de6cb8b1a790892710e3d02ebef93
Alexandre Salconi <asalconi@cimeq.qc.ca>
2021-01-25 14:43:09 -0500

    change the unit test to test only the function

--- test/unit/package-lock_prettify.cmake
+++ test/unit/package-lock_prettify.cmake
@@ -1,67 +1,23 @@
+cmake_minimum_required(VERSION 3.14 FATAL_ERROR)
 
-include(CMakePackageConfigHelpers)
+include(${CPM_PATH}/CPM.cmake)
 include(${CPM_PATH}/testing.cmake)
 
-set(TEST_BUILD_DIR ${CMAKE_CURRENT_BINARY_DIR}/package-lock_prettify)
 
-function(configureWithDeclare DECLARE_DEPENDENCY)
-  execute_process(COMMAND ${CMAKE_COMMAND} -E rm -rf ${TEST_BUILD_DIR})
-
-  if (DECLARE_DEPENDENCY)
-    set(PREPARE_CODE "CPMDeclarePackage(Dependency
-      NAME Dependency
-      SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}/local_dependency/dependency
-      UPDATE_DISCONNECTED ON
-      TESTCUSTOMDATA TRUE
-    )")
-  else()
-    set(PREPARE_CODE "")
-  endif()
-
-  configure_package_config_file(
-    "${CMAKE_CURRENT_LIST_DIR}/local_dependency/PackageLockCMakeLists.txt.in"
-    "${CMAKE_CURRENT_LIST_DIR}/local_dependency/CMakeLists.txt"
-    INSTALL_DESTINATION ${CMAKE_CURRENT_BINARY_DIR}/junk
-  )
-
-  execute_process(
-    COMMAND ${CMAKE_COMMAND} -H${CMAKE_CURRENT_LIST_DIR}/local_dependency -B${TEST_BUILD_DIR} -DCPM_INCLUDE_ALL_IN_PACKAGE_LOCK=1
-    RESULT_VARIABLE ret
-  )
-
-  ASSERT_EQUAL(${ret} "0")
-endfunction()
-
-function(updatePackageLock)
-  execute_process(
-    COMMAND ${CMAKE_COMMAND} --build ${TEST_BUILD_DIR} --target cpm-update-package-lock
-    RESULT_VARIABLE ret
-  )
-
-  ASSERT_EQUAL(${ret} "0")
-endfunction()
-
-execute_process(COMMAND ${CMAKE_COMMAND} -E rm -f ${CMAKE_CURRENT_LIST_DIR}/local_dependency/package-lock.cmake)
-configureWithDeclare(YES)
-ASSERT_NOT_EXISTS(${CMAKE_CURRENT_LIST_DIR}/local_dependency/package-lock.cmake)
-updatePackageLock()
-ASSERT_EXISTS(${CMAKE_CURRENT_LIST_DIR}/local_dependency/package-lock.cmake)
-
-file(READ ${CMAKE_CURRENT_LIST_DIR}/local_dependency/package-lock.cmake READFILE)
-
-SET(EXPECTED "# CPM Package Lock
-# This file should be committed to version control
-
-# Dependency
-CPMDeclarePackage(Dependency
+unset(PRETTY_ARGN)
+cpm_prettyfy_package_arguments(PRETTY_ARGN false
     NAME Dependency
-    SOURCE_DIR \${CMAKE_SOURCE_DIR}/dependency
+    SOURCE_DIR ${CMAKE_SOURCE_DIR}/local_dependency/dependency
+    UPDATE_DISCONNECTED ON
+    TESTCUSTOMDATA TRUE
+)
+SET(EXPECTED "    NAME Dependency
+    SOURCE_DIR \${CMAKE_SOURCE_DIR}/local_dependency/dependency
     #(unparsed)
     UPDATE_DISCONNECTED ON TESTCUSTOMDATA TRUE 
-)
 ")
+ASSERT_EQUAL(${PRETTY_ARGN} ${EXPECTED}) 
 
-ASSERT_EQUAL(${READFILE}  ${EXPECTED})
+## test comment and not commented
 
-configureWithDeclare(NO)
-execute_process(COMMAND ${CMAKE_COMMAND} -E rm -f ${CMAKE_CURRENT_LIST_DIR}/local_dependency/package-lock.cmake)
+#----
