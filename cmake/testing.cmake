d20162f59a7708cfb2b2f6d1fedfc476e711fe42
Lars Melchior <TheLartians@users.noreply.github.com>
2021-02-16 11:16:51 +0100

    add unit test

--- cmake/testing.cmake
+++ cmake/testing.cmake
@@ -10,6 +10,18 @@ function(ASSERT_EQUAL)
   endif()
 endfunction()
 
+function(ASSERT_NOT_EQUAL)
+  if(NOT ARGC EQUAL 2)
+    message(FATAL_ERROR "assertion failed: invalid argument count: ${ARGC}")
+  endif()
+
+  if(${ARGV0} STREQUAL ${ARGV1})
+    message(FATAL_ERROR "assertion failed: '${ARGV0}' == '${ARGV1}'")
+  else()
+    message(STATUS "test passed: '${ARGV0}' != '${ARGV1}'")
+  endif()
+endfunction()
+
 function(ASSERT_EMPTY)
   if(NOT ARGC EQUAL 0)
     message(FATAL_ERROR "assertion failed: input ${ARGC} not empty: '${ARGV}'")
