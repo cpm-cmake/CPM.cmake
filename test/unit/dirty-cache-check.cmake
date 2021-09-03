16678a860a9e075938bfbfa59616696c1411886c
Andrea Barbadoro <andij.cr@gmail.com>
2021-09-03 16:09:58 +0200

    test(dirty-cache-check): added unit test to check cpm_check_working_dir_is_clean
    the test creates a file in a folder and then uses git to create a repo in that folder, to check various conditions

---
+++ test/unit/dirty-cache-check.cmake
@@ -0,0 +1,58 @@
+include(${CPM_PATH}/CPM.cmake)
+include(${CPM_PATH}/testing.cmake)
+
+set(baseDir "${CMAKE_CURRENT_BINARY_DIR}/test_dirty_cache")
+set(childDir "${baseDir}/edgecase")
+
+find_package(Git REQUIRED)
+
+function(git_do dir)
+  execute_process(
+    COMMAND ${GIT_EXECUTABLE} ${ARGN}
+    RESULT_VARIABLE result
+    OUTPUT_VARIABLE status
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    WORKING_DIRECTORY "${dir}"
+  )
+  if(result)
+    message(FATAL_ERROR "git ${ARGN} fail: ${result} ${status}")
+  endif()
+endfunction()
+
+file(MAKE_DIRECTORY "${baseDir}")
+
+file(WRITE "${baseDir}/draft.txt" "this is a test")
+message(STATUS "no git, a file")
+cpm_check_working_dir_is_clean(${baseDir} nogit_test)
+assert_truthy(nogit_test)
+
+git_do("${baseDir}" init -b main)
+message(STATUS "empty repo with file")
+cpm_check_working_dir_is_clean(${baseDir} emptygit_test)
+assert_falsy(emptygit_test)
+
+git_do("${baseDir}" add draft.txt)
+git_do("${baseDir}" commit -m "test change")
+message(STATUS "commit a change")
+cpm_check_working_dir_is_clean(${baseDir} onecommit_test)
+assert_truthy(onecommit_test)
+
+file(WRITE "${baseDir}/draft.txt" "a modification")
+message(STATUS "dirty repo")
+cpm_check_working_dir_is_clean(${baseDir} nonemptygit_test)
+assert_falsy(nonemptygit_test)
+
+git_do("${baseDir}" add draft.txt)
+git_do("${baseDir}" commit -m "another change")
+message(STATUS "repo clean")
+cpm_check_working_dir_is_clean(${baseDir} twocommit_test)
+assert_truthy(twocommit_test)
+
+file(MAKE_DIRECTORY "${childDir}")
+file(WRITE "${childDir}/draft.txt" "this in another test")
+message(STATUS "a sub dir")
+cpm_check_working_dir_is_clean(${childDir} edgecase_test)
+assert_truthy(edgecase_test)
+
+
+file(REMOVE_RECURSE "${baseDir}")
