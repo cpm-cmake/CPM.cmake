20724dd7af86592aef693e63ab1a6aa28e5ca19f
Yohann <yohannassouline@hotmail.fr>
2021-09-28 14:20:03 +0200

    add sfml example

---
+++ examples/sfml/main.cpp
@@ -0,0 +1,17 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+int main()
+{
+    sf::RenderWindow win(sf::VideoMode(800, 600), "SFML window");
+
+    while (win.isOpen()) {
+        sf::Event event;
+        while (win.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) win.close();
+        }
+        win.clear(sf::Color::Black);
+        win.display();
+    }
+    return 0;
+}
