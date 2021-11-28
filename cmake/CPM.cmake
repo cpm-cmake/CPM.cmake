61a00b91f0f9f3048a85c209bd3596ceed3ea051
Lars Melchior <TheLartians@users.noreply.github.com>
2021-11-28 01:19:26 +0100

    add warning when not using release version of CPM.cmake

--- cmake/CPM.cmake
+++ cmake/CPM.cmake
@@ -57,6 +57,13 @@ See https://github.com/cpm-cmake/CPM.cmake for more information."
   endif()
 endif()
 
+if(CURRENT_CPM_VERSION MATCHES "development-version")
+  message(WARNING "Your project is using an unstable development version of CPM.cmake. \
+Please update to a recent release if possible. \
+See https://github.com/cpm-cmake/CPM.cmake for details."
+  )
+endif()
+
 set_property(GLOBAL PROPERTY CPM_INITIALIZED true)
 
 option(CPM_USE_LOCAL_PACKAGES "Always try to use `find_package` to get dependencies"
