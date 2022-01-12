473e7bf8203c05374a057e1b2ec9c974ff7b4fd0
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-01-12 06:26:08 +0200

    Latest Format.cmake. Passing style

--- test/integration/templates/using-adder/using-adder.cpp
+++ test/integration/templates/using-adder/using-adder.cpp
@@ -1,9 +1,8 @@
 #include <adder/adder.hpp>
 #include <cstdio>
 
-int main()
-{
-    int sum = adder::add(5, 3);
-    std::printf("%d\n", sum);
-    return 0;
+int main() {
+  int sum = adder::add(5, 3);
+  std::printf("%d\n", sum);
+  return 0;
 }
