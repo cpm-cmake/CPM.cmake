f6d52d74817b6c5e2b687db034ac65aa61c474ca
Lars Melchior <lars.melchior@gmail.com>
2023-02-20 19:05:52 +0100

    simplify test case and use git tag

--- test/integration/test_system_warnings.rb
+++ test/integration/test_system_warnings.rb
@@ -2,45 +2,31 @@ require_relative './lib'
 
 class SystemWarnings < IntegrationTest
   def test_dependency_added_using_system
-    prj = make_project from_template: 'using-adder'
-    prj.create_lists_from_default_template package: <<~PACK
-      # this commit has a warning in a public header
-      CPMAddPackage(
-        NAME Adder
-        GITHUB_REPOSITORY cpm-cmake/testpack-adder
-        GIT_TAG cf22d3e48d368ff268a98cfc37d4b3471b4b31c9
-        SYSTEM YES
-      )
-      # all packages using `adder` will error on warnings
-      target_compile_options(adder INTERFACE
-        $<$<CXX_COMPILER_ID:MSVC>:/Wall /WX>
-        $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wall -Werror>
-      )
-    PACK
 
-    assert_success prj.configure
-    assert_success prj.build
-  end
+    for use_system in [true, false] do
+      prj = make_project name: use_system ? "system" : "no_system", from_template: 'using-adder'
+      prj.create_lists_from_default_template package: <<~PACK
+        # this commit has a warning in a public header
+        CPMAddPackage(
+          NAME Adder
+          GITHUB_REPOSITORY cpm-cmake/testpack-adder
+          GIT_TAG v1.0.1-warnings
+          SYSTEM #{use_system ? "YES" : "NO"}
+        )
+        # all packages using `adder` will error on warnings
+        target_compile_options(adder INTERFACE
+          $<$<CXX_COMPILER_ID:MSVC>:/Wall /WX>
+          $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wall -Werror>
+        )
+      PACK
 
-  def test_dependency_added_not_using_system
-    prj = make_project from_template: 'using-adder'
-    prj.create_lists_from_default_template package: <<~PACK
-      # this commit has a warning in a public header
-      CPMAddPackage(
-        NAME Adder
-        GITHUB_REPOSITORY cpm-cmake/testpack-adder
-        GIT_TAG cf22d3e48d368ff268a98cfc37d4b3471b4b31c9
-        SYSTEM NO
-      )
-      # all packages using `adder` will error on warnings
-      target_compile_options(adder INTERFACE
-        $<$<CXX_COMPILER_ID:MSVC>:/Wall /WX>
-        $<$<NOT:$<CXX_COMPILER_ID:MSVC>>:-Wall -Werror>
-      )
-    PACK
-
-    assert_success prj.configure
-    assert_failure prj.build
+      assert_success prj.configure
+      if use_system
+        assert_success prj.build
+      else
+        assert_failure prj.build
+      end
+    end
   end
 
 end
