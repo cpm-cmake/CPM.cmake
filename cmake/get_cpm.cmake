439bfcf1349fee2dc6af966cb41f1307db2d7182
Carlo Corradini <carlo.ita98@gmail.com>
2023-09-17 21:35:11 +0200

    chore(license): update license year (#504)

--- cmake/get_cpm.cmake
+++ cmake/get_cpm.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: MIT
 #
-# SPDX-FileCopyrightText: Copyright (c) 2019-2022 Lars Melchior and contributors
+# SPDX-FileCopyrightText: Copyright (c) 2019-2023 Lars Melchior and contributors
 
 set(CPM_DOWNLOAD_VERSION 1.0.0-development-version)
 
