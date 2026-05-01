4aeb5b6a33afe1e4850482f569343b045b9845fb
Craig Hutchinson <54269136+CraigHutchinson@users.noreply.github.com>
2026-05-01 11:51:34 +0100

    style: apply cmake-format to env override unit test

--- test/unit/package-override-env.cmake
+++ test/unit/package-override-env.cmake
@@ -14,9 +14,9 @@ configure_package_config_file(
 execute_process(
   COMMAND
     ${CMAKE_COMMAND} -E env
-    "CPM_Dependency_SOURCE=${CMAKE_CURRENT_LIST_DIR}/local_dependency/dependency"
-    ${CMAKE_COMMAND} "-S${CMAKE_CURRENT_LIST_DIR}/local_dependency" "-B${TEST_BUILD_DIR}"
-    RESULT_VARIABLE ret
+    "CPM_Dependency_SOURCE=${CMAKE_CURRENT_LIST_DIR}/local_dependency/dependency" ${CMAKE_COMMAND}
+    "-S${CMAKE_CURRENT_LIST_DIR}/local_dependency" "-B${TEST_BUILD_DIR}"
+  RESULT_VARIABLE ret
 )
 
 assert_equal(${ret} "0")
